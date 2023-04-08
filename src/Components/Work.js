1211f692d7113d76a7821542e1889c36fb2e5fbd
Chandramauli <chandramauliguptach@gmail.com>
2023-04-08 22:29:59 +0530

    Changed relative path according to gh-pages build

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -5,7 +5,7 @@ export default function Work(){
         <div className="work" id="work">
            <div className="gfg">
                 <h1>Technical Content writer at GeeksforGeeks</h1>
-                <p>Wroter articles about Python and DSA with well over 20,000 views. Also improved numerous articles</p>
+                <p>Wroter articles about Python and DSA with well over 20,000 views. Also improved numerous articles.</p>
                 <button>Profile</button>
            </div>
         </div>

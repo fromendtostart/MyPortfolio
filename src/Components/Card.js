a01316dd9e15e88dd342216a92f9e15b92c5b590
Chandramauli <chandramauliguptach@gmail.com>
2023-04-08 20:05:37 +0530

    added bpx shadow to cards

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,6 +6,7 @@ export default function Card({id, title, imageurl, description, link}){
                 <div className="project-image">
                     <img src = {imageurl} alt = "project" key={id}/>
                 </div>
+                <hr></hr>
                 <div className="description">
                     <div className="title">{title}</div>
                     <div className="descriptiontext">{description}</div>

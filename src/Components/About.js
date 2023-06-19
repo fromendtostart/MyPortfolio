5eebbb7216190e6aa249f4509712ed203ea4d059
Chandramauli <chandramauliguptach@gmail.com>
2023-06-19 23:24:13 +0530

    added favicon

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ export default function About(){
     return(
         <div className="about">
             <div className="intro-section">
-                <div className = "intro-text">Hi! I'm Chandramauli. Let me take you through tour.</div>
+                <div className = "intro-text">Hi! I'm Chandramauli. Let me take you through tour!</div>
                 <div className = "nav-buttons">
                     <a href={"#projects"}><Button name = "Projects" /></a>
                     <a href={"#skills"}><Button name = "Skills" /></a>
